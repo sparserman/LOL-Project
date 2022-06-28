48022cf9efd3ba89a8e57e3dfeac071bac9c9a59
biueapple <96161368+biueapple@users.noreply.github.com>
2022-06-28 12:26:03 +0900

    베이스 스탯용값

---
+++ LOL_Client_Proj/BaseChampStat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct ChamState
+{
+    public float Attack;
+    public float AttackSpeed;
+    public float HPRegen;
+    public float MPRegen;
+    public float Def;
+    public float MagDef;
+    public float Damage;
+    public float HP;
+    public float MP;
+    public float Speed;
+    public float AttackRange;
+
+}
