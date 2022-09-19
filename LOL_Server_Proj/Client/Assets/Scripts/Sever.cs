2f9f5743e91e21efee6f66f9cd9a88e2a1251ea3
LockNing <chpjhch123@naver.com>
2022-09-19 12:14:26 +0900

    Sever Update

---
+++ LOL_Server_Proj/Client/Assets/Scripts/Sever.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+
+using System.Runtime.InteropServices;
+static class min
+{
+    public const int bufsize = 1024;
+
+    // main 1~16
+    public const int MAIN_LOGJOIN = 2;
+
+    // sub 1~16
+    public const int SUB_LOGJOIN_LOGIN = 1;
+
+    // detail 2의 16승까지 배수만
+    public const int DETALI_LOGIN_RESULT = 1;
+    public const int DETALI_JOIN_RESULT = 2;
+    public const int DETALI_LOGIN_SUCCESS = 4;
+    public const int DETALI_JOIN_SUCCESS = 8;
+}
+
+public class Sever : SingleTonMonobehaviour<Sever>
+{
+    // Start is called before the first frame update
+    
+    public string serverIp = "127.0.0.1";
+    Socket clientSocket = null;
+
+    Queue<socket_data> s_que;
+    Queue<socket_data>r_que;
+
+    ManualResetEvent s_event;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //클라이언트에서 사용할 소켓 준비
+        this.clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Debug.Log("Sever 접속시도");
+        //클라이언트는 바인딩할 필요 없음
+
+        //접속할 서버의 통신지점(목적지)
+        IPAddress serverIPAdress = IPAddress.Parse(this.serverIp);
+        IPEndPoint serverEndPoint = new IPEndPoint(serverIPAdress, 9000);
+
+        //서버로 연결 요청
+        try
+        {
+            Debug.Log("Connecting to Server");
+            this.clientSocket.Connect(serverEndPoint);
+            // 서버 연결 성공시 
+            ThreadCreate();     //쓰레드 생성
+        }
+        catch (SocketException e)
+        {
+            Debug.Log("Connection Failed:" + e.Message);
+        }
+    }
+
+    private void ThreadCreate()
+    {
+        //샌드 쓰레드 생성
+        Thread sendThread = new Thread(s_Thread);
+        sendThread.Start();
+        Debug.Log("Send쓰레드 실행");
+    }
+
+    private void s_Thread()
+    {
+        s_que = new Queue<socket_data>();
+        s_event = new ManualResetEvent(false);
+
+        //리시비 쓰레드 생성
+        Thread recvThread = new Thread(r_Thread);
+        recvThread.Start();
+        Debug.Log("Recv쓰레드 실행");
+
+        Packpaket();
+
+        while(true)
+        {
+            if (s_event.WaitOne() == true)  // 이벤트 발생시
+            {
+                for (int i = 0; i < s_que.Count; i++)
+                {
+                    socket_data temp = new socket_data();
+                    temp = s_que.Dequeue();
+                    //Sever.Instance.clientSocket.Send(packet, 0, packet.Length, SocketFlags.None);
+                }
+            }
+
+            //if() 스레드 탈출 조건 걸기
+            {
+
+            }
+        }
+    }
+
+    private void r_Thread()
+    {
+        while(true)
+        {
+            Recv();
+
+            // 큐에서 리시브 빼서 적용하기
+
+            // 스레드 탈출 조건 걸기
+        }
+       
+    }
+
+    public static void Send(Byte[] packet)
+    {
+        if (Sever.Instance.clientSocket == null)
+        {
+            return;
+        }
+    
+        //byte[] prefSize = new byte[1];
+        //prefSize[0] = (byte)packet.Length;    //버퍼의 가장 앞부분에 이 버퍼의 길이에 대한 정보가 있는데 이것을 
+        //Sever.Instance.clientSocket.Send(prefSize);    //먼저 보낸다.
+        Sever.Instance.clientSocket.Send(packet, 0, packet.Length, SocketFlags.None);
+
+    }
+
+    public static void Recv()
+    {
+        int m_size = 0;
+        int retval = 0;
+        byte[] m_packet = new byte[1024];
+
+        if (Sever.Instance.clientSocket == null)
+        {
+            return;
+        }
+
+        Sever.Instance.clientSocket.Receive(m_packet, sizeof(int), retval, SocketFlags.None);
+
+        m_size = BitConverter.ToInt32(m_packet, 0);
+        Debug.Log(m_size);
+
+        Sever.Instance.clientSocket.Receive(m_packet, m_size, retval, SocketFlags.None);
+
+        // 언패킹하고 큐에 넣기
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (this.clientSocket != null)
+        {
+            this.clientSocket.Close();
+            this.clientSocket = null;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class socket_data
+    {
+        public int msg;
+        public short size;
+        public short type;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 100)]
+        public char[] data;
+    }
+
+    private void Packpaket()
+    {
+        socket_data s_data = new socket_data();
+        s_data.data = new char[100];
+        string strData = "testString";
+        s_data.msg = 1;
+        s_data.size = (short)Marshal.SizeOf(typeof(socket_data));
+        s_data.type = 5;
+
+        int len = strData.Length;
+        for(int i =0;i<len;i++)
+        {
+            s_data.data[i] = strData[i];
+        }
+        byte[] packet = new byte[1];
+        StruckToBytes(s_data, ref packet);
+        //Sever.Instance.clientSocket.Send(packet, 0, packet.Length, SocketFlags.None);
+    }
+
+    private void StruckToBytes(object obj, ref byte[] packet)
+    {
+        int size = Marshal.SizeOf(obj) + sizeof(int);
+        //packet = new byte[size];
+        IntPtr ptr = Marshal.AllocHGlobal(size + 1);
+
+        int k = sizeof(int);  // 인트 크기
+        byte[] kk = new byte[k];  // 바이트 크기
+        byte[] kkk = new byte[size];  // 배열 크기
+
+        kk = BitConverter.GetBytes(k);  // kk에 인트 넣기
+
+        packet = new byte[kkk.Length + size];
+
+        Marshal.StructureToPtr(obj, ptr, false);
+        Marshal.Copy(ptr, kkk, 0, size);
+
+        Array.Copy(kk, 0, packet, 0, kk.Length);
+        Array.Copy(kkk, 0, packet, 0, kkk.Length);
+
+        //Marshal.Copy(ptr, packet, 0, size);
+        Marshal.FreeHGlobal(ptr);
+    }
+
+    public T ByteArrayToStruct<T>(byte[] buffer) where T : struct
+    {
+        int size = Marshal.SizeOf(typeof(T));
+        if (size > buffer.Length)
+        {
+            throw new Exception();
+        }
+
+        IntPtr ptr = Marshal.AllocHGlobal(size);
+        Marshal.Copy(buffer, 0, ptr, size);
+        T obj = (T)Marshal.PtrToStructure(ptr, typeof(T));
+        Marshal.FreeHGlobal(ptr);
+        return obj;
+    }
+
+    // Update is called once per frame
+}
--- LOL_Server_Proj/Client/Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/InputField.cs
+++ LOL_Server_Proj/Client/Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/InputField.cs
@@ -13,7 +13,7 @@ namespace UnityEngine.UI
     /// Turn a simple label into a interactable input field.
     /// </summary>
 
-    [AddComponentMenu("UI/Legacy/Input Field", 103)]
+    [AddComponentMenu("UI/Input Field", 31)]
     public class InputField
         : Selectable,
         IUpdateSelectedHandler,
@@ -200,12 +200,6 @@ namespace UnityEngine.UI
         protected TouchScreenKeyboard m_Keyboard;
         static private readonly char[] kSeparators = { ' ', '.', ',', '\t', '\r', '\n' };
 
-    #if UNITY_ANDROID
-        static private bool s_IsQuestDeviceEvaluated = false;
-    #endif // if UNITY_ANDROID
-
-        static private bool s_IsQuestDevice = false;
-
         /// <summary>
         /// Text Text used to display the input's value.
         /// </summary>
@@ -247,15 +241,12 @@ namespace UnityEngine.UI
         [SerializeField]
         private int m_CharacterLimit = 0;
 
-        [FormerlySerializedAs("onSubmit")]
-        [FormerlySerializedAs("m_OnSubmit")]
         [FormerlySerializedAs("m_EndEdit")]
-        [FormerlySerializedAs("m_OnEndEdit")]
         [SerializeField]
-        private SubmitEvent m_OnSubmit = new SubmitEvent();
+        private EndEditEvent m_OnEndEdit = new EndEditEvent();
 
         [SerializeField]
-        private EndEditEvent m_OnDidEndEdit = new EndEditEvent();
+        private SubmitEvent m_OnSubmit = new SubmitEvent();
 
         [FormerlySerializedAs("onValueChange")]
         [FormerlySerializedAs("m_OnValueChange")]
@@ -277,7 +268,6 @@ namespace UnityEngine.UI
         private Color m_SelectionColor = new Color(168f / 255f, 206f / 255f, 255f / 255f, 192f / 255f);
 
         [SerializeField]
-        [Multiline]
         [FormerlySerializedAs("mValue")]
         protected string m_Text = string.Empty;
 
@@ -320,7 +310,6 @@ namespace UnityEngine.UI
         private bool m_HasDoneFocusTransition = false;
         private WaitForSecondsRealtime m_WaitForSecondsRealtime;
         private bool m_TouchKeyboardAllowsInPlaceEditing = false;
-        private bool m_IsCompositionActive = false;
 
         private BaseInput input
         {
@@ -339,7 +328,6 @@ namespace UnityEngine.UI
 
         // Doesn't include dot and @ on purpose! See usage for details.
         const string kEmailSpecialCharacters = "!#$%&'*+-/=?^_`{|}~";
-        const string kOculusQuestDeviceModel = "Oculus Quest";
 
         protected InputField()
         {
@@ -686,7 +674,7 @@ namespace UnityEngine.UI
         /// ]]>
         ///</code>
         /// </example>
-        public EndEditEvent onEndEdit { get { return m_OnDidEndEdit; } set { SetPropertyUtility.SetClass(ref m_OnDidEndEdit, value); } }
+        public EndEditEvent onEndEdit { get { return m_OnEndEdit; } set { SetPropertyUtility.SetClass(ref m_OnEndEdit, value); } }
 
         /// <summary>
         /// The Unity Event to call when editing has ended
@@ -1119,21 +1107,6 @@ namespace UnityEngine.UI
 
     #endif // if UNITY_EDITOR
 
-    #if UNITY_ANDROID
-        protected override void Awake()
-        {
-            base.Awake();
-
-            if (s_IsQuestDeviceEvaluated)
-                return;
-
-            // Used for Oculus Quest 1 and 2 software keyboard regression.
-            // TouchScreenKeyboard.isInPlaceEditingAllowed is always returning true in these devices and would prevent the software keyboard from showing up if that value was used.
-            s_IsQuestDevice = SystemInfo.deviceModel == kOculusQuestDeviceModel;
-            s_IsQuestDeviceEvaluated = true;
-        }
-    #endif // if UNITY_ANDROID
-
         protected override void OnEnable()
         {
             base.OnEnable();
@@ -1167,7 +1140,7 @@ namespace UnityEngine.UI
                 m_TextComponent.UnregisterDirtyVerticesCallback(UpdateLabel);
                 m_TextComponent.UnregisterDirtyMaterialCallback(UpdateCaretMaterial);
             }
-            CanvasUpdateRegistry.DisableCanvasElementForRebuild(this);
+            CanvasUpdateRegistry.UnRegisterCanvasElementForRebuild(this);
 
             // Clear needs to be called otherwise sync never happens as the object is disabled.
             if (m_CachedInputRenderer != null)
@@ -1178,12 +1151,6 @@ namespace UnityEngine.UI
             m_Mesh = null;
 
             base.OnDisable();
-        }
-
-        protected override void OnDestroy()
-        {
-            CanvasUpdateRegistry.UnRegisterCanvasElementForRebuild(this);
-            base.OnDestroy();
         }
 
         IEnumerator CaretBlink()
@@ -1360,9 +1327,6 @@ namespace UnityEngine.UI
             switch (platform)
             {
                 case RuntimePlatform.Android:
-                    if (s_IsQuestDevice)
-                        return TouchScreenKeyboard.isSupported;
-
                     return !TouchScreenKeyboard.isInPlaceEditingAllowed;
                 default:
                     return TouchScreenKeyboard.isSupported;
@@ -1378,7 +1342,7 @@ namespace UnityEngine.UI
         // This currently only happens on Chrome OS devices (that support laptop and tablet mode).
         private bool InPlaceEditingChanged()
         {
-            return !s_IsQuestDevice && m_TouchKeyboardAllowsInPlaceEditing != TouchScreenKeyboard.isInPlaceEditingAllowed;
+            return m_TouchKeyboardAllowsInPlaceEditing != TouchScreenKeyboard.isInPlaceEditingAllowed;
         }
 
         void UpdateCaretFromKeyboard()
@@ -1966,9 +1930,6 @@ namespace UnityEngine.UI
 
         private bool IsValidChar(char c)
         {
-            if (c == 0)
-                return false;
-
             // Delete key on mac
             if ((int)c == 127)
                 return false;
@@ -2008,17 +1969,6 @@ namespace UnityEngine.UI
                 if (m_ProcessingEvent.rawType == EventType.KeyDown)
                 {
                     consumedEvent = true;
-
-                    // Special handling on OSX which produces more events which need to be suppressed.
-                    if (m_IsCompositionActive && compositionString.Length == 0)
-                    {
-                        // Suppress other events related to navigation or termination of composition sequence.
-                        if (m_ProcessingEvent.character == 0 && m_ProcessingEvent.modifiers == EventModifiers.None)
-                        {
-                            continue;
-                        }
-                    }
-
                     var shouldContinue = KeyPressed(m_ProcessingEvent);
                     if (shouldContinue == EditState.Finish)
                     {
@@ -2026,9 +1976,8 @@ namespace UnityEngine.UI
                             SendOnSubmit();
 
                         DeactivateInputField();
-                        continue;
+                        break;
                     }
-                    UpdateLabel();
                 }
 
                 switch (m_ProcessingEvent.type)
@@ -2309,7 +2258,7 @@ namespace UnityEngine.UI
             }
             else
             {
-                if (caretPositionInternal > 0 && caretPositionInternal - 1 < text.Length)
+                if (caretPositionInternal > 0)
                 {
                     m_Text = text.Remove(caretPositionInternal - 1, 1);
                     caretSelectPositionInternal = caretPositionInternal = caretPositionInternal - 1;
@@ -2478,17 +2427,10 @@ namespace UnityEngine.UI
                 m_PreventFontCallback = true;
 
                 string fullText;
-
                 if (EventSystem.current != null && gameObject == EventSystem.current.currentSelectedGameObject && compositionString.Length > 0)
-                {
-                    m_IsCompositionActive = true;
                     fullText = text.Substring(0, m_CaretPosition) + compositionString + text.Substring(m_CaretPosition);
-                }
                 else
-                {
-                    m_IsCompositionActive = false;
                     fullText = text;
-                }
 
                 string processed;
                 if (inputType == InputType.Password)
@@ -3123,7 +3065,7 @@ namespace UnityEngine.UI
             // Cache the value of isInPlaceEditingAllowed, because on UWP this involves calling into native code
             // Usually, the value only needs to be updated once when the TouchKeyboard is opened; however, on Chrome OS,
             // we check repeatedly to see if the in-place editing state has changed, so we can take action.
-            m_TouchKeyboardAllowsInPlaceEditing = !s_IsQuestDevice && TouchScreenKeyboard.isInPlaceEditingAllowed;
+            m_TouchKeyboardAllowsInPlaceEditing = TouchScreenKeyboard.isInPlaceEditingAllowed;
 
             if (TouchScreenKeyboardShouldBeUsed())
             {
